89ff4a6cf436bf2b596a2f231cfb827226e16bab
Kushlani Jayasinha <kushlanijayasinha@Kushlanis-MacBook-Pro.local>
2017-11-05 10:57:35 -0800

    Added Login, Added New User template, added table columns

--- MiltonMentor/settings.py
+++ MiltonMentor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MentorApp',
 ]
 
 MIDDLEWARE = [
@@ -73,12 +74,6 @@ WSGI_APPLICATION = 'MiltonMentor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 DATABASES = {  
     'default': {
@@ -90,6 +85,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
